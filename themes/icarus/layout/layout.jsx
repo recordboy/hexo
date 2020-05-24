b1053b0805fa7a206483b685b2bb846e73e052df
recordboy <ksj_design@naver.com>
2020-05-24 14:39:56 +0900

    수정

--- themes/icarus/layout/layout.jsx
+++ themes/icarus/layout/layout.jsx
@@ -19,7 +19,7 @@ module.exports = class extends Component {
             <body class={`is-${columnCount}-column`}>
                 <Navbar config={config} helper={helper} page={page} />
                 <div id="bg-main">
-                    <h1 class="tit"><span class="ico">🚀</span>Frontend Development Blog</h1>
+                    <h1 class="tit"><span class="ico">🌏</span>Frontend Development Blog</h1>
                     <span class="dimmed"></span>
                 </div>
                 <section class="section">
