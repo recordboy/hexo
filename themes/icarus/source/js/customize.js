dfe52b319f5e96259cf4dfc238854be36d591287
recordboy <ksj_design@naver.com>
2020-07-02 20:43:42 +0900

    함수 수정

--- themes/icarus/source/js/customize.js
+++ themes/icarus/source/js/customize.js
@@ -3,18 +3,13 @@
     if (url === 'https://recordboy.github.io/' || url === 'http://localhost:4000/') {
 
         const hero = document.querySelector('#hero');
-        const bg = hero.querySelector('.bg');
         const dimmed = hero.querySelector('.dimmed');
         const navbar = document.querySelector('.navbar');
         const navbarMenu = document.querySelector('.navbar-menu');
         const navbarLogo = document.querySelector('.navbar-logo');
         const navbarStart = document.querySelector('.navbar-start');
         const navbarEnd = document.querySelector('.navbar-end');
-        const windowHeight = window.innerHeight;
         let ratio = 0;
-        let bgInterval = null;
-        let bgIntervalNum = 0;
-        let bgIntervalRes = 0;
         
         // particles
         document.write('<script src="/js/particles.min.js"></script>');
@@ -22,7 +17,6 @@
 
         // main init
         hero.style.display = 'block';
-        // hero.style.height = windowHeight + 'px';
         navbar.style.position = 'absolute';
         navbar.style.width = '100%';
         navbar.style.backgroundColor = 'transparent';
@@ -30,17 +24,13 @@
         navbarMenu.style.backgroundColor = 'transparent';
         navbarLogo.childNodes[0].setAttribute('src', '/img/logo_white.png');
 
-        // nav color set
-        navSet(navbarStart);
-        navSet(navbarEnd);
-
         // main page open
-        window.onload = function() {
+        window.onload = () => {
             mainRatio();
             mainBgInit();
         }
 
-        window.addEventListener('scroll', function() {
+        window.addEventListener('scroll', () => {
             if (window.scrollY === 0) {
                 dimmed.style.display = 'none';
             } else {
@@ -50,35 +40,30 @@
             mainRatio();
         });
 
-        function navSet(target) {
+        const navSet = target => {
             for (let i = 0; i < target.childNodes.length; i++) {
                 target.childNodes[i].style.color = '#fff';
             }
         }
 
-        function mainRatio() {
+        const mainRatio = () => {
             ratio = Math.floor(window.scrollY / hero.clientHeight * 1000) / 1000;
-            // ratio = Math.floor(window.scrollY / windowHeight * 1000) / 1000;
         }
 
-        function mainBgSet() {
+        const mainBgSet = () => {
             dimmed.style.opacity = ratio;
         }
 
-        function mainBgInit() {
+        const mainBgInit = () => {
             if (ratio < 1 && ratio !== 0) {
                 dimmed.style.display = 'block';
                 dimmed.style.opacity = ratio;
-                // bgInterval = setInterval(function() {
-                //     bgIntervalNum++;
-                //     bgIntervalRes = Math.round(bgIntervalNum * 0.01 * 100) / 100;
-                //     if (bgIntervalNum >= ratio * 100) {
-                //         clearInterval(bgInterval);
-                //     }
-                //     dimmed.style.opacity = bgIntervalRes;
-                // }, 10);
             }
         }
+
+        // nav color set
+        navSet(navbarStart);
+        navSet(navbarEnd);
 
     }
 }());
